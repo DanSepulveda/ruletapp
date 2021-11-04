858e7b1d329a4890964c10ed1db4637c8bfcf71a
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-11-04 13:02:55 -0300

    Ready to deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const router = require('./routes/index')
 require('dotenv').config()
 require('./config/database')
+const path = require('path')
 
 const app = express()
 
@@ -10,4 +11,11 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-app.listen(4000, () => console.log('Server listening on port 4000'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    })
+}
+
+app.listen(process.env.PORT || 4000, '0.0.0.0', () => console.log('Server listening on port 4000'))
